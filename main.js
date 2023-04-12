8142a3028b53268690f4dcfd965ebc1eda66ef3e
mia336love <122865270+mia336love@users.noreply.github.com>
2023-04-13 01:55:14 +0300

    works all except the restart button

--- main.js
+++ main.js
@@ -1,1 +1,63 @@
-let array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+const card = document.querySelectorAll(".card");
+const front = document.querySelectorAll(".front");
+const container = document.querySelector(".container-images");
+const restartBtn = document.getElementById("restart");
+
+function mixCards() {
+  card.forEach((id) => {
+    // определение нового порядкового номера карт
+    const num = [...Array(card.length).keys()];
+
+    // генерация от 0 до до длины массива карт
+    const random = Math.floor(Math.random() * card.length);
+
+    id.style.order = num[random];
+  });
+}
+mixCards();
+
+function clickOnCard() {
+  for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener("click", () => {
+      // перевернуть карты
+      front[i].classList.add("flip");
+      const flippedCard = document.querySelectorAll(".flip");
+
+      // проверить, сколько карт перевернуто
+      // если 2, то блокнуть
+      if (flippedCard.length == 2) {
+        container.style.pointerEvents = "none";
+
+        setInterval(() => {
+          container.style.pointerEvents = "all";
+        }, 1000);
+        // проверить, совпали ли карты
+        match(flippedCard[0], flippedCard[1]);
+      }
+    });
+  }
+}
+clickOnCard();
+
+function match(cardOne, cardTwo) {
+  if (cardOne.dataset.index == cardTwo.dataset.index) {
+    // поменять классы
+    cardOne.classList.remove("flip");
+    cardTwo.classList.remove("flip");
+
+    cardOne.classList.add("match");
+    cardTwo.classList.add("match");
+  } else {
+    // вернуть обратно через 1-2 сек
+    setTimeout(() => {
+      cardOne.classList.remove("flip");
+      cardTwo.classList.remove("flip");
+    }, 1000);
+  }
+}
+
+// не закрывает карты
+restartBtn.addEventListener("click", () => {
+  mixCards();
+  clickOnCard();
+});
